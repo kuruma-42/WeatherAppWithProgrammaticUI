63a39d86bf5fabf6634852e1198c15b57dd3f590
yongjuncha <perfect_yon@naver.com>
2021-12-20 16:11:29 +0900

    add : temperature and city label  apply stack view and nested stack view for making temperature label and city labe In addition, make function to make temperature label property by using NSAttributedString.

--- Weathery/WeatherViewController.swift
+++ Weathery/WeatherViewController.swift
@@ -10,17 +10,27 @@ import UIKit
 class WeatherViewController: UIViewController {
     
     let backgroundView = UIImageView()
+    let rootStackView = UIStackView()
+    
+    // Search
+    let searchStackView = UIStackView()
     let locationButton = UIButton()
     let searchButton = UIButton()
     let searchTextField = UITextField()
-
+    
+    // Weather
+    let conditionImageView = UIImageView()
+    let temperatureLabel = UILabel()
+    let cityLabel = UILabel()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         style()
         layout()
     }
-
+    
 }
 
 extension WeatherViewController {
@@ -32,9 +42,19 @@ extension WeatherViewController {
          This means that no matter how large or small or how close you zoom in on the image,
          the lines, curves and points remain smooth.
          */
+        
         backgroundView.translatesAutoresizingMaskIntoConstraints = false
         backgroundView.image = UIImage(named: "day-background")
         backgroundView.contentMode = .scaleAspectFill
+        
+        rootStackView.translatesAutoresizingMaskIntoConstraints = false
+        rootStackView.axis = .vertical
+        rootStackView.alignment = .trailing
+        rootStackView.spacing = 10
+        
+        // search
+        searchStackView.translatesAutoresizingMaskIntoConstraints = false
+        searchStackView.spacing = 8
         
         locationButton.translatesAutoresizingMaskIntoConstraints = false
         locationButton.setBackgroundImage(UIImage(systemName: "location.circle.fill"), for: .normal)
@@ -47,16 +67,50 @@ extension WeatherViewController {
         searchTextField.translatesAutoresizingMaskIntoConstraints = false
         searchTextField.font = UIFont.preferredFont(forTextStyle: .title1)
         searchTextField.placeholder = "Search"
-        searchTextField.textAlignment = .right
+        searchTextField.textAlignment = .left
         searchTextField.borderStyle = .roundedRect
         searchTextField.backgroundColor = .systemFill
+        
+        // weather
+        conditionImageView.translatesAutoresizingMaskIntoConstraints = false
+        conditionImageView.image = UIImage(systemName: "sun.max")
+        conditionImageView.tintColor = .label
+        
+        temperatureLabel.translatesAutoresizingMaskIntoConstraints = false
+        temperatureLabel.attributedText = makeTemperatureText(with: "21")
+        temperatureLabel.font = UIFont.systemFont(ofSize: 80)
+        
+        cityLabel.translatesAutoresizingMaskIntoConstraints = false
+        cityLabel.text = "New York"
+        cityLabel.font = UIFont.preferredFont(forTextStyle: .largeTitle)
+    }
+    
+    private func makeTemperatureText(with temperature: String) -> NSAttributedString {
+        var boldTextAttributes = [NSAttributedString.Key: AnyObject]()
+        boldTextAttributes[.foregroundColor] = UIColor.label
+        boldTextAttributes[.font] = UIFont.boldSystemFont(ofSize: 100)
+        
+        var plainTextAttributes = [NSAttributedString.Key: AnyObject]()
+        plainTextAttributes[.font] = UIFont.boldSystemFont(ofSize: 80)
+        
+        let text = NSMutableAttributedString(string: temperature, attributes: boldTextAttributes)
+        text.append(NSAttributedString(string: "℃", attributes: plainTextAttributes))
+        
+        return text
     }
     
     func layout() {
         view.addSubview(backgroundView)
-        view.addSubview(locationButton)
-        view.addSubview(searchButton)
-        view.addSubview(searchTextField)
+        view.addSubview(rootStackView)
+        
+        rootStackView.addArrangedSubview(searchStackView)
+        rootStackView.addArrangedSubview(conditionImageView)
+        rootStackView.addArrangedSubview(temperatureLabel)
+        rootStackView.addArrangedSubview(cityLabel)
+        
+        searchStackView.addArrangedSubview(locationButton)
+        searchStackView.addArrangedSubview(searchTextField)
+        searchStackView.addArrangedSubview(searchButton)
         
         NSLayoutConstraint.activate([
             backgroundView.topAnchor.constraint(equalTo: view.topAnchor),
@@ -64,19 +118,25 @@ extension WeatherViewController {
             backgroundView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             backgroundView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
             
-            locationButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            locationButton.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
+            rootStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            rootStackView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: rootStackView.trailingAnchor, multiplier: 1),
+            
+            searchStackView.leadingAnchor.constraint(equalTo: rootStackView.leadingAnchor),
+            searchStackView.trailingAnchor.constraint(equalTo: rootStackView.trailingAnchor),
+            searchStackView.widthAnchor.constraint(equalTo: rootStackView.widthAnchor),
+            
             locationButton.widthAnchor.constraint(equalToConstant: 40),
             locationButton.heightAnchor.constraint(equalToConstant: 40),
             
-            searchButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            view.trailingAnchor.constraint(equalToSystemSpacingAfter: searchButton.trailingAnchor, multiplier: 1),
             searchButton.widthAnchor.constraint(equalToConstant: 40),
             searchButton.heightAnchor.constraint(equalToConstant: 40),
             
-            searchTextField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            searchTextField.leadingAnchor.constraint(equalToSystemSpacingAfter: locationButton.trailingAnchor, multiplier: 1),
-            searchButton.leadingAnchor.constraint(equalToSystemSpacingAfter: searchTextField.trailingAnchor, multiplier: 1)
+            conditionImageView.widthAnchor.constraint(equalToConstant: 120),
+            conditionImageView.heightAnchor.constraint(equalToConstant: 120),
+            
+            
+            
         ])
     }
 }
